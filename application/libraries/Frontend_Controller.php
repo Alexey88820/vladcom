50245d1bc21b91272c8b3daf8f1572bd111f2568
zzeraw <pekidoch@gmail.com>
2013-08-15 14:57:38 +0400

    redirects

--- application/libraries/Frontend_Controller.php
+++ application/libraries/Frontend_Controller.php
@@ -6,15 +6,23 @@ class Frontend_Controller extends MY_Controller
 	{
 		parent::__construct();
 
+		$server_name = $_SERVER['SERVER_NAME'];
 		$request_uri = $_SERVER['REQUEST_URI'];
+
+		if (strstr($request_uri, '/index.php/')) {
+			$request_uri = str_replace('/index.php/', '/', $request_uri);
+			redirect('http://' . $server_name . $request_uri, 'location', 301);
+		}
+		if (strstr($request_uri, '/index.php')) {
+			$request_uri = str_replace('/index.php', '/', $request_uri);
+			redirect('http://' . $server_name . $request_uri, 'location', 301);
+		}
+
 		if ((isset($request_uri[1])) && ($request_uri[1] == '?')) {
 			redirect(site_url(), 'location', 301);
 		}
 
-		$path = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-
-		$path = str_replace('?', '', $path);
-
+		$path = 'http://' . $server_name . $request_uri;
 		if (substr($path, -1) != '/') {
 			redirect($path . '/', 'location', 301);
 		}
