8c5cf98e6b4713860f684c80a630701ed70d052e
zzeraw <pekidoch@gmail.com>
2013-08-15 13:12:03 +0400

    add language pack

---
+++ application/language/russian/content_lang.php
@@ -0,0 +1,9 @@
+<?php
+$lang['menu_home']          = 'Главная';
+$lang['menu_howto']         = 'Инструкция';
+$lang['menu_contacts']      = 'Контакты';
+
+$lang['footer_navigation']  = 'Карта сайта';
+$lang['footer_follow']      = 'Присоединяетесь';
+
+?>
