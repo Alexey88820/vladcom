93022299ea7f01c43834f8ed28f2d1ca70cc7d5e
zzeraw <pekidoch@gmail.com>
2013-08-15 13:11:46 +0400

    add min bootstrap css

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -8,7 +8,7 @@
     <meta name="keywords" content="<?=$meta_keywords?>" />
     <meta name="author" content="Danilov Pavel" />
 
-    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/bootstrap.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/bootstrap.min.css" media="all" />
     <!-- <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/bootstrap-responsive.css" media="all" /> -->
     <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css?css=<?=time()?>" media="all" />
     <!-- <link rel="stylesheet/less" type="text/css" href="<?=base_url()?>assets/css/style.less" media="all" /> -->
